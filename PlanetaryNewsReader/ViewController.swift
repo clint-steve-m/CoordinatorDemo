235b6322b99ffe168d073f17a36a8ad7c83d4b07
clint-steve-m <clint_m@clintmejia.com>
2019-02-16 17:38:47 -0500

    Initial Commit

---
+++ PlanetaryNewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlanetaryNewsReader
+//
+//  Created by Clint Mejia on 2/16/19.
+//  Copyright © 2019 Clint Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
